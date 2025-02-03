60bfaad090f786bc6563c50e309431594534c7dd
Grey0124 <161997693+Grey0124@users.noreply.github.com>
2025-02-04 00:17:22 +0530

    second commit
    user registration as student and faculty added
    login for both added

--- RVCE_buddy/rvce_buddy_backend/src/app.js
+++ RVCE_buddy/rvce_buddy_backend/src/app.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const app = express();
 
@@ -9,10 +9,11 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// Default route
+// Routes
+app.use("/api/auth", require("./routes/authRoutes"));
+
 app.get('/', (req, res) => {
     res.send('RVCE Buddy Backend is Running');
 });
 
 module.exports = app;
- 
